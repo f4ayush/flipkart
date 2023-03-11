3c7d0a789f15a29faece8fe9d2122ade1523b12f
f4ayush <rajanand.ayush98@gmail.com>
2023-03-11 14:50:23 +0530

    updated server url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const API = axios.create({ baseURL: 'https://flipkart-5zfu.vercel.app' })
-const API = axios.create({ baseURL: 'http://localhost:8000' })
+const API = axios.create({ baseURL: 'https://flipkart-5zfu.vercel.app' })
+// const API = axios.create({ baseURL: 'http://localhost:8000' })
 API.interceptors.request.use((req) => {
     
     if (localStorage.getItem('profile')) {
